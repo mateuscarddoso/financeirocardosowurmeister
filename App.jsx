23d3bfc6bd7f6dab663d434e5c56c3357e8e64ab
mateuscarddoso <mateuscarddosointencional@gmail.com>
2026-01-28 01:37:42 +0000

    Fix: Fechar header corretamente após seletor visual de meses

--- App.jsx
+++ App.jsx
@@ -511,7 +511,8 @@ const App = () => {
               </div>
             </div>
           )}
-
+        </div>
+      </header>
 
       <main className="max-w-6xl mx-auto p-4 space-y-4 font-medium">
         {view === 'mensal' ? (
